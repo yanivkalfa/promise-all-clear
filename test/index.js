09b9c49708b39643ed72735c76fc00f74d9bd0d5
yanivkalfa <netameta1@yahoo.com>
2016-01-06 01:48:48 -0800

    test: added test coverage

--- test/index.js
+++ test/index.js
@@ -1,48 +1,80 @@
-var should = require('chai').should(),
-  scapegoat = require('../index'),
-  escape = scapegoat.escape,
-  unescape = scapegoat.unescape;
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+const expect = chai.expect;
+chai.use(chaiAsPromised);
+var should = require('chai').should();
+var all = require('./../index.js');
+var P = require('bluebird');
 
-describe('#escape', function() {
-  it('converts & into &amp;', function() {
-    escape('&').should.equal('&amp;');
+/**
+ * Testing:
+ * array of promises resolve
+ * array of promises rejection
+ *
+ * array of object resolve
+ * array of object rejection
+ */
+
+function generatePromise(toResolve){
+  return new P(function(resolve, reject){
+    setTimeout(function(){
+      if (toResolve) {
+        return resolve('resolved');
+      } else {
+        return reject('rejected');
+      }
+    }, 1000);
+  });
+}
+
+function generatePromises( object, toResolve){
+  var promises = [], quantity = 10;
+  for ( var i = 0; i < quantity; i++) {
+    if (object) {
+      if (i === 0) {
+        promises.push({p: generatePromise(toResolve), id: i});
+      } else {
+        promises.push({p: generatePromise(true), id: i});
+      }
+    } else {
+      if (i === 0) {
+        promises.push(generatePromise(toResolve));
+      } else {
+        promises.push(generatePromise(true));
+      }
+    }
+  }
+
+  return promises;
+}
+
+describe('Testing `all` method ', function() {
+
+  it('array of promises should resolve', function() {
+    this.timeout(6000);
+    var promises = generatePromises(false, true);
+    return expect(all(promises)).to.be.fulfilled;
   });
 
-  it('converts " into &quot;', function() {
-    escape('"').should.equal('&quot;');
+  it('array of promises should reject', function() {
+    this.timeout(6000);
+    var promises = generatePromises(false, false);
+    return expect(all(promises)).to.be.rejected;
   });
 
-  it('converts  into &#39;', function() {
-    escape('').should.equal('&#39;');
+  it('array of objects should resolve and have `err` and `res` properties', function() {
+    this.timeout(6000);
+    var promises = generatePromises(true, true);
+    return expect(all(promises)).to.be.fulfilled.and.to.eventually.satisfy( function(resp) {
+      return (resp[0].hasOwnProperty('err') && resp[0].hasOwnProperty('res'));
+    });
   });
 
-  it('converts < into &lt;', function() {
-    escape('<').should.equal('&lt;');
-  });
-
-  it('converts > into &gt;', function() {
-    escape('>').should.equal('&gt;');
-  });
-});
-
-describe('#unescape', function() {
-  it('converts &amp; into &', function() {
-    unescape('&amp;').should.equal('&');
-  });
-
-  it('converts &quot; into "', function() {
-    unescape('&quot;').should.equal('"');
-  });
-
-  it('converts &#39; into ', function() {
-    unescape('&#39;').should.equal('');
-  });
-
-  it('converts &lt; into <', function() {
-    unescape('&lt;').should.equal('<');
-  });
-
-  it('converts &gt; into >', function() {
-    unescape('&gt;').should.equal('>');
+  it('array of objects should reject and have `err` and `res` properties', function() {
+    this.timeout(6000);
+    var promises = generatePromises(true, false);
+    return expect(all(promises)).to.be.rejected.and.to.eventually.satisfy( function(resp) {
+      return (resp[0].hasOwnProperty('err') && resp[0].hasOwnProperty('res'));
+    });
   });
 });
